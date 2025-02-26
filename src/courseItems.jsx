817b468346093efd86669cbcac4c14ce8bec7dca
Alperen Emre Candan <91540339+alperenec@users.noreply.github.com>
2025-02-26 11:45:12 +0300

    init

---
+++ src/courseItems.jsx
@@ -0,0 +1,38 @@
+import Image1 from './assets/1.jpg';
+import Image2 from './assets/2.jpg';
+import Image3 from './assets/3.jpg';
+import Image4 from './assets/4.avif';
+
+
+const courseItems = [
+  {
+    id: 'adsadsadsadsada',
+    title: 'C Programlama Eğitimi',
+    price: 19,  // 
+    img: Image1,
+    quantity: 1,
+  },
+  {
+    id: 'sadsadsadsadsadsadsad',
+    title: 'C# Eğitimi',
+    price: 69,
+    img: Image2,
+    quantity: 1,
+  },
+  {
+    id: 'sadsadsadsadsadsadsadsa',
+    title: 'Typescript Eğitimi',
+    price: 619,
+    img: Image3,
+    quantity: 1,
+  },
+  {
+    id: 'adsadsadsadsadsadsadsaf',
+    title: 'React Eğitimi',
+    price: 129,
+    img: Image4,
+    quantity: 1,
+  },
+];
+
+export default courseItems;
